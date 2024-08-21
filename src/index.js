f8a99534b43fe2379ac889e7d169f587816ac52d
Fuujin <133795177+jhoncarlnayao@users.noreply.github.com>
2024-08-22 00:03:17 +0800

    MINIMAL UPDATE

--- src/index.js
+++ src/index.js
@@ -9,15 +9,16 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    // width: 800,
+    // height: 600,
+    fullscreen: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'intro.html'));
+  mainWindow.loadFile(path.join(__dirname, 'style.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
